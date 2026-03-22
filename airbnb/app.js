76575ae348f79808b1b046415731c98801221d09
Tarpan <tarpansaikia@gmail.com>
2026-03-22 15:58:46 +0530

    Tailwind CSS

--- airbnb/app.js
+++ airbnb/app.js
@@ -18,6 +18,8 @@ app.use(userRouter);
 
 app.use("/host",hostRouter);
 
+app.use(express.static(path.join(rootDir,'public')))
+
 app.use((req,res,next)=>{
   res.status(404).sendFile(path.join(rootDir,'views','404.html'));;
 })
